da7074015e7db8daf68f3ad06f432a9ed98681a0
Kyle N <123481268+PLAknazaXM@users.noreply.github.com>
2023-05-17 21:10:50 -0400

    Add files via upload

---
+++ Pages/Lab_33.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly_express as px
+import plotly.figure_factory as ff
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+st.set_page_config(page_title="Lab 33",
+                   page_icon=":test_tube:", layout="wide")
+
+st.title('Lab 33: The Soluble Group')
+
+st.text('''This lab tests for Ammonium and Sodium Ions.''')
+
+st.subheader('''Ammonium Test:
+
+Warning!  When completing a stiff test, heated chemicals can splatter out!''')
+
+st.text('''1: Moisten a strip of red litmus paper with distilled water, place on bottom of watch glass.
+2: Place 3mL of test solution in evaporation dish, add 1-2 mL of 6M NaOH.
+3: Quickly cover dish with watch glass, gently warm with hand or warm water bath.
+   Check for a blue spot on the litmus paper.
+   
+Result: If a blue spot on litmus paper forms or the smell of ammonia is present, ammonium was in the compound.''')
+
+st.subheader('''Sodium Test:
+
+Notice: Test is very sensitive, let nothing come into contact with wire after cleaning!''')
+
+st.text('''
+1: Use 6M HCl to clean a nickel-chromium alloy wire until no color is present in a flame test.
+2: Dip clean wire into test solution, hold to outer region of flame.
+
+Result: If a luminous, fluffy, yellow-orage flame is persistent for around 30 seconds, sodium ions are present.''')
---
+++ Qualitative_Analysis.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly_express as px
+import plotly.figure_factory as ff
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+st.set_page_config(page_title="Qualitative Analysis Lab",
+                   page_icon=":test_tube:", layout="wide")
+
+st.sidebar.success("Select a page above")
+
+st.title('Qualitative Analysis Labs')
+
+st.subheader('''Select lab being done on the side.''')
